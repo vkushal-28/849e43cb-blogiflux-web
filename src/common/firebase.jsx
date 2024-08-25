96bcc9798b13350def7e69f477ac6a8879454d2e
Kushal Vala <kushalvala@Kushals-MacBook-Pro.local>
2024-08-25 13:09:19 -0400

    restore gh-pages configuration

--- src/common/firebase.jsx
+++ src/common/firebase.jsx
@@ -4,7 +4,7 @@ import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-console.log(import.meta.env.VITE_API_KEY);
+
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_API_KEY,
